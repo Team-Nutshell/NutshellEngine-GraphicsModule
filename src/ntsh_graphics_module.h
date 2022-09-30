bfb01ccce22b54bd966edb704b873c0138b35ec1
ntsh-oni <teamnutshell.dev@gmail.com>
2022-09-30 19:47:27 +0200

    initial graphics module

---
+++ src/ntsh_graphics_module.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../external/Common/module_interfaces/ntsh_graphics_module_interface.h"
+
+class NutshellGraphicsModule : public NutshellGraphicsModuleInterface {
+public:
+    NutshellGraphicsModule() : NutshellGraphicsModuleInterface("Nutshell Graphics Test Module") {}
+
+    void init();
+    void update(double dt);
+    void destroy();
+};
