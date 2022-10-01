d37b28f5cc8d9bc6c9d7f0185e6e69fb3fc97dcc
ZaOniRinku <zaonirinku@gmail.com>
2022-10-01 16:08:48 +0200

    replaces spaces for tabs

--- src/ntsh_graphics_module.cpp
+++ src/ntsh_graphics_module.cpp
@@ -4,22 +4,22 @@
 #include "../external/Common/ntsh_engine_enums.h"
 
 void NutshellGraphicsModule::init() {
-    NTSH_MODULE_WARNING("init() function not implemented.");
+	NTSH_MODULE_WARNING("init() function not implemented.");
 }
 
 void NutshellGraphicsModule::update(double dt) {
-    NTSH_UNUSED(dt);
-    NTSH_MODULE_WARNING("update() function not implemented.");
+	NTSH_UNUSED(dt);
+	NTSH_MODULE_WARNING("update() function not implemented.");
 }
 
 void NutshellGraphicsModule::destroy() {
-    NTSH_MODULE_WARNING("destroy() function not implemented.");
+	NTSH_MODULE_WARNING("destroy() function not implemented.");
 }
 
 extern "C" NTSH_MODULE_API NutshellGraphicsModuleInterface * createModule() {
-    return new NutshellGraphicsModule;
+	return new NutshellGraphicsModule;
 }
 
 extern "C" NTSH_MODULE_API void destroyModule(NutshellGraphicsModuleInterface * m) {
-    delete m;
+	delete m;
 }
