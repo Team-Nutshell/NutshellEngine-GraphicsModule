48657880246ecc719e37680a640295acb01fad4c
ntsh-oni <teamnutshell.dev@gmail.com>
2022-10-14 13:59:32 +0200

    initial vulkan instance creation

--- src/ntsh_graphics_module.cpp
+++ src/ntsh_graphics_module.cpp
@@ -1,11 +1,80 @@
 #include "ntsh_graphics_module.h"
-#include "../external/Module/ntsh_module_defines.h"
 #include "../external/Module/ntsh_dynamic_library.h"
-#include "../external/Common/ntsh_engine_defines.h"
-#include "../external/Common/ntsh_engine_enums.h"
 
 void NutshellGraphicsModule::init() {
-	NTSH_MODULE_WARNING("init() function not implemented.");
+	VkApplicationInfo applicationInfo = {};
+	applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	applicationInfo.pNext = nullptr;
+	applicationInfo.pApplicationName = "NutshellEngine Vulkan Triangle Graphics Module";
+	applicationInfo.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
+	applicationInfo.pEngineName = "NutshellEngine";
+	applicationInfo.engineVersion = VK_MAKE_VERSION(0, 0, 1);
+	applicationInfo.apiVersion = VK_API_VERSION_1_1;
+
+	VkInstanceCreateInfo instanceCreateInfo = {};
+	instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instanceCreateInfo.pNext = nullptr;
+	instanceCreateInfo.flags = 0;
+	instanceCreateInfo.pApplicationInfo = &applicationInfo;
+#ifdef NTSH_DEBUG
+	std::array<const char*, 1> explicitLayers = { "VK_LAYER_KHRONOS_validation" };
+	bool foundValidationLayer = false;
+	uint32_t propertyCount;
+	NTSH_VK_CHECK(vkEnumerateInstanceLayerProperties(&propertyCount, nullptr));
+	std::vector<VkLayerProperties> properties(propertyCount);
+	NTSH_VK_CHECK(vkEnumerateInstanceLayerProperties(&propertyCount, properties.data()));
+
+	for (const VkLayerProperties& availableLayer : properties) {
+		if (strcmp(availableLayer.layerName, "VK_LAYER_KHRONOS_validation") == 0) {
+			foundValidationLayer = true;
+			break;
+		}
+	}
+
+	if (foundValidationLayer) {
+		instanceCreateInfo.enabledLayerCount = 1;
+		instanceCreateInfo.ppEnabledLayerNames = explicitLayers.data();
+	}
+	else {
+		NTSH_MODULE_WARNING("Could not find validation layer VK_LAYER_KHRONOS_validation.");
+		instanceCreateInfo.enabledLayerCount = 0;
+		instanceCreateInfo.ppEnabledLayerNames = nullptr;
+	}
+#else
+	instanceCreateInfo.enabledLayerCount = 0;
+	instanceCreateInfo.ppEnabledLayerNames = nullptr;
+#endif
+	std::vector<const char*> extensions;
+#if NTSH_DEBUG
+	extensions.push_back("VK_EXT_debug_utils");
+#endif
+	if (m_windowModule) {
+		extensions.push_back("VK_KHR_surface");
+		instanceCreateInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+		instanceCreateInfo.ppEnabledExtensionNames = extensions.data();
+	}
+	else {
+		instanceCreateInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+		instanceCreateInfo.ppEnabledExtensionNames = extensions.data();
+	}
+	NTSH_VK_CHECK(vkCreateInstance(&instanceCreateInfo, nullptr, &m_instance));
+
+#ifdef NTSH_DEBUG
+	VkDebugUtilsMessengerCreateInfoEXT debugMessengerCreateInfo = {};
+	debugMessengerCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+	debugMessengerCreateInfo.pNext = nullptr;
+	debugMessengerCreateInfo.flags = 0;
+	debugMessengerCreateInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+	debugMessengerCreateInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	debugMessengerCreateInfo.pfnUserCallback = debugCallback;
+	debugMessengerCreateInfo.pUserData = nullptr;
+
+	auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(m_instance, "vkCreateDebugUtilsMessengerEXT");
+	NTSH_VK_CHECK(func(m_instance, &debugMessengerCreateInfo, nullptr, &m_debugMessenger));
+#endif
 }
 
 void NutshellGraphicsModule::update(double dt) {
@@ -14,7 +83,12 @@ void NutshellGraphicsModule::update(double dt) {
 }
 
 void NutshellGraphicsModule::destroy() {
-	NTSH_MODULE_WARNING("destroy() function not implemented.");
+#ifdef NTSH_DEBUG
+	auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(m_instance, "vkDestroyDebugUtilsMessengerEXT");
+	func(m_instance, m_debugMessenger, nullptr);
+#endif
+
+	vkDestroyInstance(m_instance, nullptr);
 }
 
 extern "C" NTSH_MODULE_API NutshellGraphicsModuleInterface* createModule() {
--- src/ntsh_graphics_module.h
+++ src/ntsh_graphics_module.h
@@ -1,5 +1,32 @@
 #pragma once
 #include "../external/Common/module_interfaces/ntsh_graphics_module_interface.h"
+#include "../external/Common/ntsh_engine_defines.h"
+#include "../external/Common/ntsh_engine_enums.h"
+#include "../external/Module/ntsh_module_defines.h"
+#include "vulkan/vulkan.h"
+
+#define NTSH_VK_CHECK(f) \
+	do { \
+		int64_t check = f; \
+		if (check) { \
+			NTSH_MODULE_ERROR("Vulkan Error.\nError code: " + std::to_string(check) + "\nFile: " + std::string(__FILE__) + "\nFunction: " + #f + "\nLine: " + std::to_string(__LINE__), NTSH_RESULT_UNKNOWN_ERROR); \
+		} \
+	} while(0)
+
+#define NTSH_VK_VALIDATION(m) \
+	do { \
+		NTSH_MODULE_WARNING("Vulkan Validation Layer: " + std::string(m)); \
+	} while(0)
+
+VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT * pCallbackData, void* pUserData) {
+	NTSH_UNUSED(messageSeverity);
+	NTSH_UNUSED(messageType);
+	NTSH_UNUSED(pUserData);
+	
+	NTSH_VK_VALIDATION(pCallbackData->pMessage);
+
+	return VK_FALSE;
+}
 
 class NutshellGraphicsModule : public NutshellGraphicsModuleInterface {
 public:
@@ -8,4 +35,10 @@ public:
 	void init();
 	void update(double dt);
 	void destroy();
+
+private:
+	VkInstance m_instance;
+#ifdef NTSH_DEBUG
+	VkDebugUtilsMessengerEXT m_debugMessenger;
+#endif
 };
