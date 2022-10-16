2485c68f1a276fde80b7cf85cc3be331353d702f
ZaOniRinku <zaonirinku@gmail.com>
2022-10-16 19:40:32 +0200

    replace functions non implemented warnings

--- src/ntsh_graphics_module.cpp
+++ src/ntsh_graphics_module.cpp
@@ -5,16 +5,16 @@
 #include "../external/Common/ntsh_engine_enums.h"
 
 void NutshellGraphicsModule::init() {
-	NTSH_MODULE_WARNING("init() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 void NutshellGraphicsModule::update(double dt) {
 	NTSH_UNUSED(dt);
-	NTSH_MODULE_WARNING("update() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 void NutshellGraphicsModule::destroy() {
-	NTSH_MODULE_WARNING("destroy() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 extern "C" NTSH_MODULE_API NutshellGraphicsModuleInterface* createModule() {
