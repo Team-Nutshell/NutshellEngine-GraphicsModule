db538635c80f5f4210687f43f630a841e2fd7f66
ntsh-oni <teamnutshell.dev@gmail.com>
2023-01-11 15:47:11 +0100

    update submodule and add the load functions

--- src/ntsh_graphics_module.h
+++ src/ntsh_graphics_module.h
@@ -8,4 +8,9 @@ public:
 	void init();
 	void update(double dt);
 	void destroy();
+
+	// Loads the mesh described in the mesh parameter in the internal format and returns a unique identifier
+	NtshMeshId load(const NtshMesh mesh);
+	// Loads the image described in the image parameter in the internal format and returns a unique identifier
+	NtshImageId load(const NtshImage image);
 };
