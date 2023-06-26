27815893336b0e53d2a77ea2198ae6307839ee4b
ntsh-oni <teamnutshell.dev@gmail.com>
2023-06-26 17:31:43 +0200

    normalize namespace utilization in header

--- src/ntshengn_graphics_module.h
+++ src/ntshengn_graphics_module.h
@@ -5,16 +5,16 @@ namespace NtshEngn {
 
 	class GraphicsModule : public GraphicsModuleInterface {
 	public:
-		GraphicsModule() : GraphicsModuleInterface("NutshellEngine Graphics Test Module") {}
+		GraphicsModule() : GraphicsModuleInterface("NutshellEngine Default Graphics Module") {}
 
 		void init();
 		void update(double dt);
 		void destroy();
 
 		// Loads the mesh described in the mesh parameter in the internal format and returns a unique identifier
-		NtshEngn::MeshId load(const NtshEngn::Mesh& mesh);
+		MeshId load(const Mesh& mesh);
 		// Loads the image described in the image parameter in the internal format and returns a unique identifier
-		NtshEngn::ImageId load(const NtshEngn::Image& image);
+		ImageId load(const Image& image);
 	};
 
 }
