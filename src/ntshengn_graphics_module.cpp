3cda67110c6d1b3f1dd578ef54e75f7a30010399
ntsh-oni <teamnutshell.dev@gmail.com>
2023-07-08 14:02:53 +0200

    put common resources outside of external directory

--- src/ntshengn_graphics_module.cpp
+++ src/ntshengn_graphics_module.cpp
@@ -1,8 +1,8 @@
 #include "ntshengn_graphics_module.h"
-#include "../external/Module/utils/ntshengn_module_defines.h"
-#include "../external/Module/utils/ntshengn_dynamic_library.h"
-#include "../external/Common/utils/ntshengn_defines.h"
-#include "../external/Common/utils/ntshengn_enums.h"
+#include "../Module/utils/ntshengn_module_defines.h"
+#include "../Module/utils/ntshengn_dynamic_library.h"
+#include "../Common/utils/ntshengn_defines.h"
+#include "../Common/utils/ntshengn_enums.h"
 
 void NtshEngn::GraphicsModule::init() {
 	NTSHENGN_MODULE_FUNCTION_NOT_IMPLEMENTED();
