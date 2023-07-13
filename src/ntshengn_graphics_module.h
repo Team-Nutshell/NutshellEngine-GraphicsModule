2f56088109204c026191b8bf6008afbc6afac6e3
ntsh-oni <teamnutshell.dev@gmail.com>
2023-07-13 21:18:08 +0200

    cache samplers

--- src/ntshengn_graphics_module.h
+++ src/ntshengn_graphics_module.h
@@ -15,6 +15,7 @@
 #undef None
 #undef Success
 #endif
+#include <string>
 #include <vector>
 #include <limits>
 #include <unordered_map>
@@ -72,7 +73,7 @@ struct InternalMesh {
 
 struct InternalTexture {
 	uint32_t imageIndex = 0;
-	uint32_t samplerIndex = 0;
+	std::string samplerKey = 0;
 };
 
 struct InternalMaterial {
@@ -174,7 +175,7 @@ namespace NtshEngn {
 		void resize();
 
 		// Create sampler
-		uint32_t createSampler(const ImageSampler& sampler);
+		std::string createSampler(const ImageSampler& sampler);
 
 		// Attribute an InternalObject index
 		uint32_t attributeObjectIndex();
@@ -324,7 +325,7 @@ namespace NtshEngn {
 		std::vector<VkImage> m_textureImages;
 		std::vector<VmaAllocation> m_textureImageAllocations;
 		std::vector<VkImageView> m_textureImageViews;
-		std::vector<VkSampler> m_textureSamplers;
+		std::unordered_map<std::string, VkSampler> m_textureSamplers;
 		std::unordered_map<const Image*, uint32_t> m_imageAddresses;
 		std::vector<InternalTexture> m_textures;
 
