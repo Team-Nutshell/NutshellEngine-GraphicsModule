c5a0bf0fa86c1aae7ee07bd2e9c7566b7443bf06
ntsh-oni <teamnutshell.dev@gmail.com>
2023-08-13 12:03:32 +0200

    implement drawUILine and drawUIRectangle

--- src/ntshengn_graphics_module.h
+++ src/ntshengn_graphics_module.h
@@ -79,6 +79,10 @@ namespace NtshEngn {
 
 		// Draws a text on the UI with the font in the fontID parameter using the position on screen and color
 		void drawUIText(FontID fontID, const std::string& text, const Math::vec2& position, const Math::vec4& color);
+		// Draws a line on the UI according to its start and end points and its color
+		void drawUILine(const Math::vec2& start, const Math::vec2& end, const Math::vec4& color);
+		// Draws a rectangle on the UI according to its position, its size (width and height) and its color
+		void drawUIRectangle(const Math::vec2& position, const Math::vec2& size, const Math::vec4& color);
 
 	public:
 		const ComponentMask getComponentMask() const;
