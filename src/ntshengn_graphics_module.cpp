b1829ee8ab705ce243feab6ca5c9f5aba963bc66
ntsh-oni <teamnutshell.dev@gmail.com>
2023-08-15 09:09:01 +0200

    implement drawUIImage

--- src/ntshengn_graphics_module.cpp
+++ src/ntshengn_graphics_module.cpp
@@ -809,6 +809,15 @@ void NtshEngn::GraphicsModule::drawUIRectangle(const Math::vec2& position, const
 	NTSHENGN_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
+void NtshEngn::GraphicsModule::drawUIImage(ImageID imageID, ImageSamplerFilter imageSamplerFilter, const Math::vec2& position, float rotation, const Math::vec2& scale) {
+	NTSHENGN_UNUSED(imageID);
+	NTSHENGN_UNUSED(imageSamplerFilter);
+	NTSHENGN_UNUSED(position);
+	NTSHENGN_UNUSED(rotation);
+	NTSHENGN_UNUSED(scale);
+	NTSHENGN_MODULE_FUNCTION_NOT_IMPLEMENTED();
+}
+
 VkSurfaceCapabilitiesKHR NtshEngn::GraphicsModule::getSurfaceCapabilities(size_t index) {
 	VkPhysicalDeviceSurfaceInfo2KHR surfaceInfo = {};
 	surfaceInfo.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_SURFACE_INFO_2_KHR;
