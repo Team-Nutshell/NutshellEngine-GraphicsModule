d3be9a2d13ae7b6bf5e85601deabe11ea23c46f2
ntsh-oni <teamnutshell.dev@gmail.com>
2023-08-30 16:10:52 +0200

    minor fix

--- src/ntshengn_graphics_module.cpp
+++ src/ntshengn_graphics_module.cpp
@@ -282,6 +282,7 @@ void NtshEngn::GraphicsModule::init() {
 	}
 	// Or create an image to draw on
 	else {
+		m_drawImageFormat = VK_FORMAT_R8G8B8A8_SRGB;
 		m_imageCount = 1;
 
 		m_viewport.x = 0.0f;
@@ -1871,11 +1872,11 @@ void NtshEngn::GraphicsModule::createSwapchain(VkSwapchainKHR oldSwapchain) {
 	}
 
 	std::vector<VkSurfaceFormatKHR> surfaceFormats = getSurfaceFormats();
-	m_swapchainFormat = surfaceFormats[0].format;
+	m_drawImageFormat = surfaceFormats[0].format;
 	VkColorSpaceKHR swapchainColorSpace = surfaceFormats[0].colorSpace;
 	for (const VkSurfaceFormatKHR& surfaceFormat : surfaceFormats) {
 		if (surfaceFormat.format == VK_FORMAT_B8G8R8A8_SRGB && surfaceFormat.colorSpace == VK_COLORSPACE_SRGB_NONLINEAR_KHR) {
-			m_swapchainFormat = surfaceFormat.format;
+			m_drawImageFormat = surfaceFormat.format;
 			swapchainColorSpace = surfaceFormat.colorSpace;
 			break;
 		}
@@ -1915,7 +1916,7 @@ void NtshEngn::GraphicsModule::createSwapchain(VkSwapchainKHR oldSwapchain) {
 	swapchainCreateInfo.flags = 0;
 	swapchainCreateInfo.surface = m_surface;
 	swapchainCreateInfo.minImageCount = minImageCount;
-	swapchainCreateInfo.imageFormat = m_swapchainFormat;
+	swapchainCreateInfo.imageFormat = m_drawImageFormat;
 	swapchainCreateInfo.imageColorSpace = swapchainColorSpace;
 	swapchainCreateInfo.imageExtent = swapchainExtent;
 	swapchainCreateInfo.imageArrayLayers = 1;
@@ -1947,7 +1948,7 @@ void NtshEngn::GraphicsModule::createSwapchain(VkSwapchainKHR oldSwapchain) {
 		swapchainImageViewCreateInfo.flags = 0;
 		swapchainImageViewCreateInfo.image = m_swapchainImages[i];
 		swapchainImageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-		swapchainImageViewCreateInfo.format = m_swapchainFormat;
+		swapchainImageViewCreateInfo.format = m_drawImageFormat;
 		swapchainImageViewCreateInfo.components.r = VK_COMPONENT_SWIZZLE_R;
 		swapchainImageViewCreateInfo.components.g = VK_COMPONENT_SWIZZLE_G;
 		swapchainImageViewCreateInfo.components.b = VK_COMPONENT_SWIZZLE_B;
@@ -2326,10 +2327,7 @@ std::vector<uint32_t> NtshEngn::GraphicsModule::compileShader(const std::string&
 
 void NtshEngn::GraphicsModule::createGraphicsPipeline() {
 	// Create graphics pipeline
-	VkFormat pipelineRenderingColorFormat = VK_FORMAT_R8G8B8A8_SRGB;
-	if (windowModule && windowModule->isOpen(windowModule->getMainWindowID())) {
-		pipelineRenderingColorFormat = m_swapchainFormat;
-	}
+	VkFormat pipelineRenderingColorFormat = m_drawImageFormat;
 
 	VkPipelineRenderingCreateInfo pipelineRenderingCreateInfo = {};
 	pipelineRenderingCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO;
@@ -2758,10 +2756,7 @@ void NtshEngn::GraphicsModule::createUITextResources() {
 	NTSHENGN_VK_CHECK(vkCreateDescriptorSetLayout(m_device, &descriptorSetLayoutCreateInfo, nullptr, &m_uiTextDescriptorSetLayout));
 
 	// Create graphics pipeline
-	VkFormat pipelineRenderingColorFormat = VK_FORMAT_R8G8B8A8_SRGB;
-	if (windowModule && windowModule->isOpen(windowModule->getMainWindowID())) {
-		pipelineRenderingColorFormat = m_swapchainFormat;
-	}
+	VkFormat pipelineRenderingColorFormat = m_drawImageFormat;
 
 	VkPipelineRenderingCreateInfo pipelineRenderingCreateInfo = {};
 	pipelineRenderingCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO;
@@ -3097,10 +3092,7 @@ void NtshEngn::GraphicsModule::updateUITextDescriptorSet(uint32_t frameInFlight)
 
 void NtshEngn::GraphicsModule::createUILineResources() {
 	// Create graphics pipeline
-	VkFormat pipelineRenderingColorFormat = VK_FORMAT_R8G8B8A8_SRGB;
-	if (windowModule && windowModule->isOpen(windowModule->getMainWindowID())) {
-		pipelineRenderingColorFormat = m_swapchainFormat;
-	}
+	VkFormat pipelineRenderingColorFormat = m_drawImageFormat;
 
 	VkPipelineRenderingCreateInfo pipelineRenderingCreateInfo = {};
 	pipelineRenderingCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO;
@@ -3324,10 +3316,7 @@ void NtshEngn::GraphicsModule::createUILineResources() {
 
 void NtshEngn::GraphicsModule::createUIRectangleResources() {
 	// Create graphics pipeline
-	VkFormat pipelineRenderingColorFormat = VK_FORMAT_R8G8B8A8_SRGB;
-	if (windowModule && windowModule->isOpen(windowModule->getMainWindowID())) {
-		pipelineRenderingColorFormat = m_swapchainFormat;
-	}
+	VkFormat pipelineRenderingColorFormat = m_drawImageFormat;
 
 	VkPipelineRenderingCreateInfo pipelineRenderingCreateInfo = {};
 	pipelineRenderingCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO;
@@ -3580,10 +3569,7 @@ void NtshEngn::GraphicsModule::createUIImageResources() {
 	NTSHENGN_VK_CHECK(vkCreateDescriptorSetLayout(m_device, &descriptorSetLayoutCreateInfo, nullptr, &m_uiImageDescriptorSetLayout));
 
 	// Create graphics pipeline
-	VkFormat pipelineRenderingColorFormat = VK_FORMAT_R8G8B8A8_SRGB;
-	if (windowModule && windowModule->isOpen(windowModule->getMainWindowID())) {
-		pipelineRenderingColorFormat = m_swapchainFormat;
-	}
+	VkFormat pipelineRenderingColorFormat = m_drawImageFormat;
 
 	VkPipelineRenderingCreateInfo pipelineRenderingCreateInfo = {};
 	pipelineRenderingCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO;
