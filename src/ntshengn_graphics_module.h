59152a8173c66ec778243122142b281d562a6db6
ntsh-oni <teamnutshell.dev@gmail.com>
2023-09-11 23:56:01 +0200

    add animation functions

--- src/ntshengn_graphics_module.h
+++ src/ntshengn_graphics_module.h
@@ -64,6 +64,16 @@ namespace NtshEngn {
 		// Loads the font described in the font parameter in the internal format and returns a unique identifier
 		FontID load(const Font& font);
 
+		// Plays an animation for an entity, indexed in the entity's model animation list
+		void playAnimation(Entity entity, uint32_t animationIndex);
+		// Pauses an animation played by an entity
+		void pauseAnimation(Entity entity);
+		// Stops an animation played by an entity
+		void stopAnimation(Entity entity);
+
+		// Returns true if the entity is currently playing the animation with index animationIndex, else, returns false
+		bool isAnimationPlaying(Entity entity, uint32_t animationIndex);
+
 		// Draws a text on the UI with the font in the fontID parameter using the position on screen and color
 		void drawUIText(FontID fontID, const std::string& text, const Math::vec2& position, const Math::vec4& color);
 		// Draws a line on the UI according to its start and end points and its color
