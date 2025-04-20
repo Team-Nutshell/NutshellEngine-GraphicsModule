e8c5d7cdc7f5614c76698b6da75941f61e1609db
ntsh-oni <teamnutshell.dev@gmail.com>
2025-04-20 18:52:12 +0200

    add distance to lights

--- src/ntshengn_graphics_module.cpp
+++ src/ntshengn_graphics_module.cpp
@@ -785,6 +785,7 @@ void NtshEngn::GraphicsModule::update(float dt) {
 		InternalLight internalLight;
 		internalLight.position = Math::vec4(lightTransform.position, 0.0f);
 		internalLight.color = Math::vec4(lightLight.color, lightLight.intensity);
+		internalLight.cutoff.z = lightLight.distance;
 
 		memcpy(reinterpret_cast<char*>(m_lightBuffers[m_currentFrameInFlight].address) + offset, &internalLight, sizeof(InternalLight));
 		offset += sizeof(InternalLight);
@@ -817,7 +818,7 @@ void NtshEngn::GraphicsModule::update(float dt) {
 		internalLight.position = Math::vec4(lightTransform.position, 0.0f);
 		internalLight.direction = Math::vec4(lightDirection, 0.0f);
 		internalLight.color = Math::vec4(lightLight.color, lightLight.intensity);
-		internalLight.cutoff = Math::vec4(lightLight.cutoff, 0.0f, 0.0f);
+		internalLight.cutoff = Math::vec4(lightLight.cutoff, lightLight.distance, 0.0f);
 
 		memcpy(reinterpret_cast<char*>(m_lightBuffers[m_currentFrameInFlight].address) + offset, &internalLight, sizeof(InternalLight));
 		offset += sizeof(InternalLight);
@@ -3539,6 +3540,7 @@ void NtshEngn::GraphicsModule::createRayTracingPipeline() {
 			vec3 color;
 			float intensity;
 			vec2 cutoff;
+			float distance;
 		};
 
 		struct Vertex {
@@ -3954,17 +3956,23 @@ void NtshEngn::GraphicsModule::createRayTracingPipeline() {
 				float intensity = 1.0;
 				float distance = 10000.0;
 
+				bool inLightDistance = true;
+
 				// Directional Light
 				if (lightIndex < lights.count.x) {
 					l = -lights.info[lightIndex].direction;
+
 					lc = lights.info[lightIndex].color * lights.info[lightIndex].intensity;
 				}
 				// Point Light
 				else if (lightIndex < (lights.count.x + lights.count.y)) {
 					l = normalize(lights.info[lightIndex].position - worldPosition);
+
 					distance = length(lights.info[lightIndex].position - worldPosition);
 					float attenuation = 1.0 / (distance * distance);
 					lc = (lights.info[lightIndex].color * lights.info[lightIndex].intensity) * attenuation;
+
+					inLightDistance = lights.info[lightIndex].distance >= distance;
 				}
 				// Spot Light
 				else {
@@ -3974,16 +3982,21 @@ void NtshEngn::GraphicsModule::createRayTracingPipeline() {
 					}
 
 					l = normalize(lights.info[lightIndex].position - worldPosition);
+
+					distance = length(lights.info[lightIndex].position - worldPosition);
 					lc = lights.info[lightIndex].color * lights.info[lightIndex].intensity;
 					float theta = dot(l, -lights.info[lightIndex].direction);
 					float epsilon = cos(lights.info[lightIndex].cutoff.y) - cos(lights.info[lightIndex].cutoff.x);
 					intensity = clamp((theta - cos(lights.info[lightIndex].cutoff.x)) / epsilon, 0.0, 1.0);
 					intensity = 1.0 - intensity;
-					distance = length(lights.info[lightIndex].position - worldPosition);
+
+					inLightDistance = lights.info[lightIndex].distance >= distance;
 				}
 
-				directLighting = (shade(n, v, l, lc * intensity, d * intensity, metalnessSample, roughnessSample) / float(lightCount)) * shadows(l, distance);
-				directLighting *= occlusionSample;
+				if (inLightDistance) {
+					directLighting = (shade(n, v, l, lc * intensity, d * intensity, metalnessSample, roughnessSample) / float(lightCount)) * shadows(l, distance);
+					directLighting *= occlusionSample;
+				}
 			}
 
 			// Ambient Lights
