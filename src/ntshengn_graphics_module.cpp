8e8b0d6ba463ef39a020797626da77f47b9e868d
ntsh-oni <teamnutshell.dev@gmail.com>
2025-05-24 11:07:36 +0200

    re-use default textures when none are specified in material

--- src/ntshengn_graphics_module.cpp
+++ src/ntshengn_graphics_module.cpp
@@ -6591,6 +6591,9 @@ void NtshEngn::GraphicsModule::loadRenderableForEntity(Entity entity) {
 			material.diffuseTextureIndex = addToTextures({ imageID, samplerKey });
 		}
 	}
+	else {
+		material.diffuseTextureIndex = 0;
+	}
 	if (renderable.material.normalTexture.image) {
 		bool textureChanged = false;
 
@@ -6609,6 +6612,9 @@ void NtshEngn::GraphicsModule::loadRenderableForEntity(Entity entity) {
 		if (textureChanged) {
 			material.normalTextureIndex = addToTextures({ imageID, samplerKey });
 		}
+	}
+	else {
+		material.normalTextureIndex = 1;
 	}
 	if (renderable.material.metalnessTexture.image) {
 		bool textureChanged = false;
@@ -6629,6 +6635,9 @@ void NtshEngn::GraphicsModule::loadRenderableForEntity(Entity entity) {
 			material.metalnessTextureIndex = addToTextures({ imageID, samplerKey });
 		}
 	}
+	else {
+		material.metalnessTextureIndex = 2;
+	}
 	if (renderable.material.roughnessTexture.image) {
 		bool textureChanged = false;
 
@@ -6647,6 +6656,9 @@ void NtshEngn::GraphicsModule::loadRenderableForEntity(Entity entity) {
 		if (textureChanged) {
 			material.roughnessTextureIndex = addToTextures({ imageID, samplerKey });
 		}
+	}
+	else {
+		material.roughnessTextureIndex = 3;
 	}
 	if (renderable.material.occlusionTexture.image) {
 		bool textureChanged = false;
@@ -6667,6 +6679,9 @@ void NtshEngn::GraphicsModule::loadRenderableForEntity(Entity entity) {
 			material.occlusionTextureIndex = addToTextures({ imageID, samplerKey });
 		}
 	}
+	else {
+		material.occlusionTextureIndex = 4;
+	}
 	if (renderable.material.emissiveTexture.image) {
 		bool textureChanged = false;
 
@@ -6685,6 +6700,9 @@ void NtshEngn::GraphicsModule::loadRenderableForEntity(Entity entity) {
 		if (textureChanged) {
 			material.emissiveTextureIndex = addToTextures({ imageID, samplerKey });
 		}
+	}
+	else {
+		material.emissiveTextureIndex = 5;
 	}
 	if (renderable.material.emissiveFactor != material.emissiveFactor) {
 		material.emissiveFactor = renderable.material.emissiveFactor;
