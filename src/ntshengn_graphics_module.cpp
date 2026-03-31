29089513041bda931821866325d4d0c192c6f2da
ntsh-oni <teamnutshell.dev@gmail.com>
2026-03-31 16:46:11 +0200

    vertex color as vec4

--- src/ntshengn_graphics_module.cpp
+++ src/ntshengn_graphics_module.cpp
@@ -3574,7 +3574,7 @@ void NtshEngn::GraphicsModule::createRayTracingPipeline() {
 			vec3 position;
 			vec3 normal;
 			vec2 uv;
-			vec3 color;
+			vec4 color;
 			vec4 tangent;
 			vec4 joints;
 			vec4 weights;
