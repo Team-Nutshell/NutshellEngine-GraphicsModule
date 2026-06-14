6c402c4315aedb773f26eb85052ae4b2c19bdd9c
ntsh-oni <teamnutshell.dev@gmail.com>
2026-06-14 19:48:06 +0200

    rotation as quaternion

--- src/ntshengn_graphics_module.cpp
+++ src/ntshengn_graphics_module.cpp
@@ -539,10 +539,7 @@ void NtshEngn::GraphicsModule::update(float dt) {
 		const Camera& camera = ecs->getComponent<Camera>(m_mainCamera);
 		const Transform& cameraTransform = ecs->getComponent<Transform>(m_mainCamera);
 
-		const Math::mat4 cameraRotation = Math::rotate(cameraTransform.rotation.x, Math::vec3(1.0f, 0.0f, 0.0f)) *
-			Math::rotate(cameraTransform.rotation.y, Math::vec3(0.0f, 1.0f, 0.0f)) *
-			Math::rotate(cameraTransform.rotation.z, Math::vec3(0.0f, 0.0f, 1.0f));
-		Math::mat4 cameraView = cameraRotation * Math::lookAtRH(cameraTransform.position, cameraTransform.position + camera.forward, camera.up);
+		const Math::mat4 cameraView = Math::quatToRotationMatrix(cameraTransform.rotation) * Math::lookAtRH(cameraTransform.position, cameraTransform.position + camera.forward, camera.up);
 		Math::mat4 cameraProjection = Math::perspectiveRH(camera.fov, m_viewport.width / m_viewport.height, camera.nearPlane, camera.farPlane);
 		cameraProjection[1][1] *= -1.0f;
 		std::array<Math::mat4, 2> cameraMatrices{ cameraView, cameraProjection };
